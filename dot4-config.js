5abe35a011023b45706f51178a0e314fa174c1aa
Tobias Ceska <tobias.ceska@realtech.com>
2019-12-05 16:18:52 +0100

    added copyrights

--- dot4-config.js
+++ dot4-config.js
@@ -1,3 +1,10 @@
+/**
+ * @copyright Copyright (C) REALTECH AG, Germany - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by Tobias Ceska <tobias.ceska@realtech.com>, December 2019
+ */
+
 module.exports = function(RED) {
     function Dot4Config(n) {
         RED.nodes.createNode(this,n);
